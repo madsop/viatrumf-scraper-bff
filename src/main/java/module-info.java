ba1a92ea8c20d3b3c0c1b472ce014f28aeb9f560
Mads Opheim <mads.opheim@gmail.com>
2020-05-23 22:19:34 +0200

    Switching to more intellij-friendly requires

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,8 +7,8 @@ module viatrumf.scraper.bff {
     requires com.google.api.apicommon;
     requires google.cloud.firestore;
     requires google.cloud.core;
-    requires jakarta.enterprise.cdi.api;
-    requires jakarta.inject.api;
     requires microprofile.config.api;
     requires java.annotation;
+    requires cdi.api;
+    requires javax.inject;
 }

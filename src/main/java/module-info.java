29e102dc711f57e6373114cd04ad91c9db498f1f
Mads Opheim <mads.opheim@gmail.com>
2020-05-23 14:35:31 +0200

    Module info file

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module viatrumf.scraper.bff {
+    requires java.compiler;
+    requires java.ws.rs;
+    requires java.xml.bind;
+    requires com.google.api.apicommon;
+    requires google.cloud.firestore;
+    requires google.cloud.core;
+    requires jakarta.enterprise.cdi.api;
+    requires jakarta.inject.api;
+    requires microprofile.config.api;
+    requires java.annotation;
+}

a377016760fca33c5223f99cf32069fdb82d7a9c
Mads Opheim <mads.opheim@gmail.com>
2026-08-01 16:11:03 +0200

    Skulle ikkje så mykje til for å få på plass ein første enkel variant av sas online shopping

--- src/main/java/no/madsopheim/viatrumf/scraper/bff/Nettbutikk.kt
+++ src/main/java/no/madsopheim/viatrumf/scraper/bff/Nettbutikk.kt
@@ -2,12 +2,9 @@ package no.madsopheim.viatrumf.scraper.bff
 
 import java.time.LocalDateTime
 
-// TODO: Denne er tilpassa strukturen i trumf netthandel
-// Vil ikkje funke for SAS online shopping utan endringar
 data class Nettbutikk(
     val namn: String,
     val href: String,
-    val popularitet: String,
     val verdi: String,
     val timestamp: LocalDateTime,
     val kategori: String?
@@ -15,8 +12,7 @@ data class Nettbutikk(
     constructor(stringObjectMap: Map<String, Any>) : this(
         namn = stringObjectMap["namn"].toString(),
         href = stringObjectMap["href"].toString(),
-        popularitet = stringObjectMap["popularitet"].toString(),
-        verdi = stringObjectMap["verdi"].toString(),
+        verdi = stringObjectMap["verdi"]?.toString()?.takeIf { it.isNotEmpty() } ?: stringObjectMap["points"].toString(),
         timestamp = LocalDateTime.parse(stringObjectMap["timestamp"].toString(), datePattern),
         kategori = stringObjectMap["kategori"]?.toString()?.takeIf { it.isNotEmpty() }
     )

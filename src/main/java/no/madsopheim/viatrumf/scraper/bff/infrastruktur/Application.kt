5775f85a13a6f7b82177cc36b37ad0dc75ce1f0e
Mads Opheim <mads.opheim@gmail.com>
2026-08-01 14:03:53 +0200

    Bruker ikkje verken openapi eller liveness aktivt, så kan like godt ta det bort igjen

--- src/main/java/no/madsopheim/viatrumf/scraper/bff/infrastruktur/Application.kt
+++
@@ -1,8 +0,0 @@
-package no.madsopheim.viatrumf.scraper.bff.infrastruktur
-
-import jakarta.ws.rs.core.Application
-import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition
-import org.eclipse.microprofile.openapi.annotations.info.Info
-
-@OpenAPIDefinition(info = Info(title = "Viatrumf-scraper-bff", version = "1.0"))
-class Application : Application()
